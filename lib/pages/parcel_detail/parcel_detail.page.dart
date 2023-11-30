2a9751fe970e99b633eb7738eb943f59190cf753
0yco <enzo.daumalle@epitech.eu>
2023-11-30 18:05:31 +0100

    Feat calculate iC-Apex, hydric constraint, and Feat DataTable

--- lib/pages/parcel_detail/parcel_detail.page.dart
+++ lib/pages/parcel_detail/parcel_detail.page.dart
@@ -18,12 +18,6 @@ class ParcelDetailPage extends StatefulWidget {
 }
 
 class _ParcelDetailPageState extends State<ParcelDetailPage> {
-  String _formatDate(String timestamp) {
-    final date = DateTime.parse(timestamp);
-    final formattedDate = DateFormat.MMMMd('fr').format(date);
-    return formattedDate;
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,33 +32,131 @@ class _ParcelDetailPageState extends State<ParcelDetailPage> {
           },
         ),
       ),
-      body: ListView.builder(
-        itemCount: widget.sessions?.length ?? 0,
-        itemBuilder: (context, index) {
-          final session = widget.sessions![index];
-          return ListTile(
-            title: Text(_formatDate(session.sessionDate)),
-            // Add more details about the session here
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            buildSessionsBoard(context),
+          ],
+        ),
+      ),
+      floatingActionButton: buildFloatingActionButton(context),
+    );
+  }
+
+  DataTable buildSessionsBoard(BuildContext context) {
+    // TODO: Déplacer dans un service
+    String _formatDate(String timestamp) {
+      final date = DateTime.parse(timestamp);
+      final formattedDate = DateFormat.MMMMd('fr').format(date);
+      return formattedDate;
+    }
+
+    // TODO: Déplacer dans un service
+    double calculateIcApex(
+        int apexFullGrowth, int apexSlowerGrowth, int apexStuntedGrowth) {
+      int sumApex = apexFullGrowth + apexSlowerGrowth + apexStuntedGrowth;
+      return (apexFullGrowth + (apexSlowerGrowth / 2)) / sumApex;
+    }
+
+    // TODO: Déplacer dans un service
+    String calculateHydricConstraint(int apexFullGrowth, int apexSlowerGrowth,
+        int apexStuntedGrowth, isPruned) {
+      int sumApex = apexFullGrowth + apexSlowerGrowth + apexStuntedGrowth;
+      double moyenne = calculateIcApex(apexFullGrowth, apexSlowerGrowth, apexStuntedGrowth);
+      double tauxApexFullGrowth = apexFullGrowth / sumApex * 100;
+      double tauxApexStuntedGrowth = apexStuntedGrowth / sumApex * 100;
+      // TODO: A voir si on garde le ifvClasse
+      int ifvClasse = -1;
+
+      if (isPruned) {
+        ifvClasse = 4;
+        return 'Ecimée';
+      }
+      if (moyenne >= 0.75) {
+        ifvClasse = 0;
+        return 'Absente';
+      } else if (tauxApexFullGrowth >= 5) {
+        ifvClasse = 1;
+        return 'Faible';
+      } else if (tauxApexStuntedGrowth <= 90) {
+        ifvClasse = 2;
+        return 'Modérée';
+      }
+      ifvClasse = 3;
+      return 'Sévère';
+    }
+
+    Future<dynamic> showNoteDialog(BuildContext context) {
+      return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Note'),
+            content: const Text(
+                'La contrainte hydrique est calculée à partir de la moyenne des iC-Apex. '
+                'Si la parcelle est écimée, la contrainte hydrique est égale à "Ecimée".'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Fermer'),
+              ),
+            ],
           );
         },
-      ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ElevatedApexButton(icon: Symbols.cut, callback: () => {}),
-          const SizedBox(width: 10),
-          ElevatedApexButton(
-            text: 'Nouvelle Session',
-            callback: () => {
-                  Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => const CreateUpdateSession(
-                      title: 'Nouvelle session',
+      );
+    }
+
+    return DataTable(
+      columns: const [
+        DataColumn(label: Text('Date')),
+        DataColumn(label: Text('iC-Apex')),
+        DataColumn(label: Text('C.H.'), tooltip: 'Contrainte hydrique (C.H.)'),
+        DataColumn(label: Text('')),
+      ],
+      rows: widget.sessions?.map<DataRow>((session) {
+            return DataRow(
+              cells: [
+                DataCell(Text(_formatDate(session.sessionDate))),
+                DataCell(Text(calculateIcApex(48, 3, 1).toStringAsFixed(2))),
+                DataCell(Text(calculateHydricConstraint(48, 3, 1, false))),
+                DataCell(
+                  Container(
+                    color: Theme.of(context).colorScheme.primary.withOpacity(0.1),
+                    padding: const EdgeInsets.all(8),
+                    child: IconButton(
+                      icon: Icon(Icons.article, color: Theme.of(context).colorScheme.primary),
+                      onPressed: () {
+                        showNoteDialog(context);
+                      },
                     ),
-                  ))
-                }
-            ),
-        ],
-      ),
+                  ),
+                ),
+              ],
+            );
+          }).toList() ??
+          [],
+    );
+  }
+
+  Row buildFloatingActionButton(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ElevatedApexButton(icon: Symbols.cut, callback: () => {}),
+        const SizedBox(width: 10),
+        ElevatedApexButton(
+          text: 'Nouvelle Session',
+          callback: () => {
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => const CreateUpdateSession(
+                title: 'Nouvelle session',
+              ),
+            ))
+          },
+        ),
+      ],
     );
   }
 }
