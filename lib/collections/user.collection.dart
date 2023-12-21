a69930bd1a771ed95851d85414ecf33dbdb7b120
0yco <enzo.daumalle@epitech.eu>
2023-12-21 15:41:55 +0100

    Update user collection

--- lib/collections/user.collection.dart
+++ lib/collections/user.collection.dart
@@ -7,35 +7,17 @@ part 'user.collection.g.dart';
 class User {
 
   @Index(unique: true, replace: true)
-  @Name('id_utilisateur')
   late String id;
 
   Id get isarId => fastHash(id);
 
-  @Name('prenom')
   late String firstname;
 
-  @Name('nom')
   late String lastname;
 
-  @Name('email')
   late String email;
 
-  @Name('structure')
   late String structure;
 
-  @Name('date_creation')
-  late int creationDate;
-
-  @Name('date_maj')
-  late int updateDate;
-
-  @Name('token')
-  late String? token;
-
-  @Name('model_ifv')
   late int ifvModel;
-
-  @Name('etat')
-  late int state;
 }
