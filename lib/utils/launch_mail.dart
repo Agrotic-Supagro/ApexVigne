0e215cd552aacf8cad01bfe43c70b132aa1cd582
0yco <enzo.daumalle@epitech.eu>
2024-03-13 16:11:03 +0100

    Update email and subject

--- lib/utils/launch_mail.dart
+++ lib/utils/launch_mail.dart
@@ -1,7 +1,7 @@
 import 'package:url_launcher/url_launcher_string.dart';
 
 void launchMail() async {
-  const mailUrl = 'mailto:test@gmail.com?subject=Nous contacter';
+  const mailUrl = 'mailto:leo.pichon@gmail.com?subject=ApeX Vigne - ';
 
   try {
     await launchUrlString(mailUrl);
