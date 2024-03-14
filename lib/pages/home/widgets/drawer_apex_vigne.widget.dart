1463ea0351ee3434df7ccedf09ff28780d3fc0dc
0yco <enzo.priv.30@gmail.com>
2024-03-14 15:21:17 +0100

    Add partners and copyright in drawer

--- lib/pages/home/widgets/drawer_apex_vigne.widget.dart
+++ lib/pages/home/widgets/drawer_apex_vigne.widget.dart
@@ -13,74 +13,104 @@ class DrawerApexVigne extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-          DrawerHeader(
-            decoration: BoxDecoration(
-              color: Theme.of(context).colorScheme.primary,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: Column(
+                      children: [
+                        const Image(
+                            image: AssetImage('assets/images/logos/logo_apex_vigne_transparent.png'),
+                            fit: BoxFit.cover,
+                            height: 70,
+                        ),
+                        Text(AppLocalizations.of(context)!.appName,
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                ListTile(
+                  title: Text(AppLocalizations.of(context)!.titleProfile),
+                  trailing: Icon(Symbols.chevron_right, color: Colors.grey.shade400),
+                  onTap: () {
+                    Navigator.pop(context);
+                    Navigator.of(context).push(
+                      MaterialPageRoute<void>(
+                        builder: (BuildContext context) => const ProfilPage(),
+                      ),
+                    );
+                  },
+                ),
+                Divider(
+                  color: Colors.grey.shade300,
+                  indent: 10.0,
+                  endIndent: 30.0,
+                  height: 10.0,
+                ),
+                ListTile(
+                  title: Text(AppLocalizations.of(context)!.titleContact),
+                  trailing: Icon(Symbols.open_in_new, color: Colors.grey.shade400),
+                  onTap: () {
+                    launchMail();
+                  },
+                ),
+                Divider(
+                  color: Colors.grey.shade300,
+                  indent: 10.0,
+                  endIndent: 30.0,
+                  height: 10.0,
+                ),
+                ListTile(
+                  title: Text(AppLocalizations.of(context)!.titleAbout),
+                  trailing: Icon(Symbols.chevron_right, color: Colors.grey.shade400),
+                  onTap: () {
+                    Navigator.pop(context);
+                    Navigator.of(context).push(
+                      MaterialPageRoute<void>(
+                        builder: (BuildContext context) => const AboutPage(),
+                      ),
+                    );
+                  },
+                ),
+              ],
             ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10),
-              child: Column(
-                children: [
-                  const Image(
-                      image: AssetImage('assets/images/logos/logo_apex_vigne_transparent.png'),
-                      fit: BoxFit.cover,
-                      height: 70),
-                  Text(AppLocalizations.of(context)!.appName,
-                    style: const TextStyle(color: Colors.white))
-                  // SizedBox(height: 20),
-                  // Image(
-                  //     image: AssetImage('assets/images/logos/logo_iam.png'),
-                  //     fit: BoxFit.cover,
-                  //     height: 40),
-                ],
+          ),
+          const Row(
+            children: [
+              SizedBox(width: 20),
+              Image(
+                image: AssetImage('assets/images/logos/logo_ifv.png'),
+                width: 90,
               ),
+              SizedBox(width: 15),
+              Image(
+                image: AssetImage('assets/images/logos/logo_iam.png'),
+                width: 140,
+              ),
+              SizedBox(width: 20, height: 60),
+            ],
+          ),
+          const Padding(
+            padding: EdgeInsets.only(bottom: 10),
+            child: Text(
+              '© 2024 ApeX Vigne',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 12,
+              ),
+              textAlign: TextAlign.center,
             ),
-          ),
-          ListTile(
-            title: Text(AppLocalizations.of(context)!.titleProfile),
-            trailing: Icon(Symbols.chevron_right, color: Colors.grey.shade400),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.of(context).push(
-                MaterialPageRoute<void>(
-                  builder: (BuildContext context) => const ProfilPage(),
-                ),
-              );
-            },
-          ),
-          Divider(
-            color: Colors.grey.shade300,
-            indent: 10.0,
-            endIndent: 30.0,
-            height: 10.0,
-          ),
-          ListTile(
-            title: Text(AppLocalizations.of(context)!.titleContact),
-            trailing: Icon(Symbols.open_in_new, color: Colors.grey.shade400),
-            onTap: () {
-              launchMail();
-            },
-          ),
-          Divider(
-            color: Colors.grey.shade300,
-            indent: 10.0,
-            endIndent: 30.0,
-            height: 10.0,
-          ),
-          ListTile(
-            title: Text(AppLocalizations.of(context)!.titleAbout),
-            trailing: Icon(Symbols.chevron_right, color: Colors.grey.shade400),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.of(context).push(
-                MaterialPageRoute<void>(
-                  builder: (BuildContext context) => const AboutPage(),
-                ),
-              );
-            },
           ),
         ],
       ),
