4f7678c750a10081f21d6431b80bb62dce11cd53
0yco <enzo.daumalle@epitech.eu>
2024-03-25 11:26:49 +0100

    Feat export sessions

--- lib/pages/profile/profile.page.dart
+++ lib/pages/profile/profile.page.dart
@@ -3,6 +3,7 @@ import 'package:apex_vigne/main.dart';
 import 'package:apex_vigne/pages/login/login.page.dart';
 import 'package:apex_vigne/pages/profile/widgets/list_tile.widget.dart';
 import 'package:apex_vigne/services/isar.service.dart';
+import 'package:apex_vigne/services/sessions_api.service.dart';
 import 'package:apex_vigne/shared_widgets/elevated_apex_button.widget.dart';
 import 'package:apex_vigne/shared_widgets/offline_dialog.dart';
 import 'package:flutter/material.dart';
@@ -19,6 +20,7 @@ class ProfilPage extends StatefulWidget {
 
 class _ProfilPageState extends State<ProfilPage> {
   final AuthenticationService auth = AuthenticationService();
+  final SessionsApiService sessionsApiService = SessionsApiService();
 
   @override
   Widget build(BuildContext context) {
@@ -30,6 +32,7 @@ class _ProfilPageState extends State<ProfilPage> {
         actions: [
           if (!AuthenticationService().isOnlineState.value)
             _buildOfflineButton(context),
+          _buildShareButton(context),
           _buildLanguageButton(context),
         ],
       ),
@@ -75,6 +78,15 @@ class _ProfilPageState extends State<ProfilPage> {
           offlineDialog(context);
         },
       ),
+    );
+  }
+
+  Widget _buildShareButton(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Symbols.share),
+      onPressed: () async {
+        await sessionsApiService.exportSessions(context);
+      },
     );
   }
 
