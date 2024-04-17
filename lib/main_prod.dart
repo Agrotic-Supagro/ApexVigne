9f7fddc1c0cb1a7422aa88c72c26cabfeb2d96ea
Charles Moszkowicz <charles.moszkowicz@gmail.com>
2024-04-17 15:37:23 +0200

    change api url

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -2,8 +2,8 @@ import 'package:apex_vigne/app_config.dart';
 import 'package:apex_vigne/main_common.dart';
 
 Future<void> main() async {
-  //AppConfig.apiBaseUrl = 'http://api.apexvigne.agrotic.org:81';
-  AppConfig.apiBaseUrl = 'http://veneo5.servadmin.fr:81';
+  AppConfig.apiBaseUrl = 'https://api.apexvigne.agrotic.org';
+  //AppConfig.apiBaseUrl = 'http://veneo5.servadmin.fr:81';
   AppConfig.debugShowCheckedModeBanner = false;
   await mainCommon();
 }
